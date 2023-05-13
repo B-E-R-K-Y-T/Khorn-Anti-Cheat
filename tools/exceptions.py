686263beb66c586df7962a09648c4e9ad1adbc03
Tim <BerkyT115@yandex.ru>
2023-05-14 02:38:03 +0300

    Blood, for the God of blood!

--- tools/exceptions.py
+++ tools/exceptions.py
@@ -1,1 +1,31 @@
-class InvalidDir(Exception): ...
+class Messages:
+    InvalidDir_DEFAULT_MSG = 'Invalid directory.'
+
+
+class DefaultException(Exception):
+    def __init__(self, *args, default):
+        self.default = default
+
+        if args:
+            self.message = args[0]
+        else:
+            self.message = None
+
+    def __str__(self):
+        return self.__generate_error_message()
+
+    def __generate_error_message(self):
+        if self.message:
+            return f'{self.__class__.__name__}: {self.message}'
+        else:
+            return f'{self.__class__.__name__}: {self.default}'
+
+
+class InvalidDir(DefaultException):
+    def __init__(self, *args):
+        super().__init__(*args, default=Messages.InvalidDir_DEFAULT_MSG)
+
+
+__all__ = (
+    InvalidDir.__name__,
+)
